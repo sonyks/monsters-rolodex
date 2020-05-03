f8e3a5c5098acbbea44de741004dbfb951a9406e
Oleksandr Poliukhovych <O.Poliukhovych@wolterskluwer.com>
2020-05-04 00:02:22 +0300

    Updates

---
+++ precache-manifest.7a6a9a06cd7a061dcb03bdd01ddefc2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66eb8ed48a9ea0cc288377e10d3e6655",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f834c8b08c4eeb5ca6b9",
+    "url": "/monsters-rolodex/static/css/main.cb668630.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f834c8b08c4eeb5ca6b9",
+    "url": "/monsters-rolodex/static/js/main.4f8977c4.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
